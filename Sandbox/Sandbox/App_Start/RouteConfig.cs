333a8996139d053ed8f707e21b96e8132e19e9ef
tosca ragnini <tosca.ragnini@gmail.com>
2015-05-06 21:26:09 -0500

    changed the shared views

--- Sandbox/Sandbox/App_Start/RouteConfig.cs
+++ Sandbox/Sandbox/App_Start/RouteConfig.cs
@@ -11,18 +11,53 @@ namespace Sandbox
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
-           routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
-         //   routes.MapRoute(
-           //     name: "Default",
-            //    url: "{controller}/{action}/{id}",
-             //   defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-          // );
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.IgnoreRoute("{*favicon}", new { favicon = @"(.*/)?favicon.ico(/.*)?" });
+            routes.IgnoreRoute("{folder}/{*pathInfo}", new { folder = "Content" });
 
             routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Main", action = "Index", id = UrlParameter.Optional });
+               "Default",
+               "{controller}/{action}/{id}",
+                new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+               "Index",
+               "Index",
+               new { controller = "Home", action = "Index" }
+               );
+
+            routes.MapRoute(
+               "Scribbles",
+               "Scribbles",
+               new { controller = "Home", action = "Scribbles" }
+               );
+
+            routes.MapRoute(
+               "Kids-Rings",
+               "Kids-Rings",
+               new { controller = "Home", action = "KidsRings" }
+               );
+
+            routes.MapRoute(
+               "The-Mother-Load-Project",
+               "The-Mother-Load-Project",
+               new { controller = "Home", action = "TheMotherLoadProject" }
+               );
+
+            routes.MapRoute(
+               "About-Natalie-Macellaio",
+               "About-Natalie-Macellaio",
+               new { controller = "Home", action = "About" }
+               );
+
+            routes.MapRoute(
+               "Contact-Natalie-Macellaio",
+               "Contact-Natalie-Macellaio",
+               new { controller = "Home", action = "Contact" }
+               );
+
+            
         }
     }
 }
--- Sandbox/Sandbox/Controllers/HomeController.cs
+++ Sandbox/Sandbox/Controllers/HomeController.cs
@@ -8,9 +8,36 @@ namespace Sandbox.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        //
+        // GET: /Home/
+
+       public ActionResult Index()
         {
-           
+            return View();
+       }
+        public ActionResult Scribbles()
+        {
+
+            return View();
+        }
+        public ActionResult KidsRings()
+        {
+
+            return View();
+        }
+        public ActionResult TheMotherLoadProject()
+        {
+
+            return View();
+        }
+        public ActionResult About()
+        {
+
+            return View();
+        }
+        public ActionResult Contact()
+        {
+
             return View();
         }
      }   
